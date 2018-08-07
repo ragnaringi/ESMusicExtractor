458e103985cd272f3d0a4eed1ab26bc26112a612
ragnaringi <ragnar.hrafnkelsson@gmail.com>
2018-08-07 17:42:43 +0100

    Inital commit

---
+++ ESMusicExtractor/ESMusicExtractor.h
@@ -0,0 +1,19 @@
+//
+//  ESMusicExtractor.h
+//  ESMusicExtractor
+//
+//  Created by Ragnar Hrafnkelsson on 07/08/2018.
+//  Copyright © 2018 Reactify. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ESMusicExtractor.
+FOUNDATION_EXPORT double ESMusicExtractorVersionNumber;
+
+//! Project version string for ESMusicExtractor.
+FOUNDATION_EXPORT const unsigned char ESMusicExtractorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ESMusicExtractor/PublicHeader.h>
+
+
